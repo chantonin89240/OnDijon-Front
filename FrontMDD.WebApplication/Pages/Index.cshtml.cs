486c6fd4d9a44ea130144a4ac991b19368319959
Morgan <morgan.goncalves@diiage.org>
2023-05-17 15:08:34 +0200

    add history request from post remove at 8 entry

--- FrontMDD.WebApplication/Pages/Index.cshtml.cs
+++ FrontMDD.WebApplication/Pages/Index.cshtml.cs
@@ -1,8 +1,11 @@
 ﻿using FrontMDD.Entities;
 using FrontMDD.Service;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 using System.Reflection.Metadata;
+using System.Text;
 
 namespace FrontMDD.WebApplication.Pages
 {
@@ -11,23 +14,33 @@ namespace FrontMDD.WebApplication.Pages
         private readonly ILogger<IndexModel> _logger;
         private readonly AbrisServices _abrisServices;
         private readonly AbrisStatServices _abrisStatServices;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
 
         public List<Abris>? Abris { get; set; }
         public List<ShelterState>? ShelterState { get; set; }
         public int? AbrisStatCount { get; set; }
         public string libelleResult { get; set; }
+        public List<string> HistorySearch { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger, AbrisServices abrisServices, AbrisStatServices abrisStatServices)
+        public IndexModel(ILogger<IndexModel> logger, AbrisServices abrisServices, AbrisStatServices abrisStatServices, IHttpContextAccessor httpContextAccessor)
         {
             _logger = logger;
             _abrisServices = abrisServices;
             _abrisStatServices = abrisStatServices;
+            _httpContextAccessor = httpContextAccessor;
+            HistorySearch = new List<string>();
+
 
         }
 
         public async Task OnGetAsync()
         {
+
+        
+
+
+
             Abris = await _abrisServices.GetAllAbris();
             if (Abris != null)
             {
@@ -40,7 +53,8 @@ namespace FrontMDD.WebApplication.Pages
                     abris.NbPlaces = shelter?.Available;
                 }
             }
-            
+         
+
         }
 
         public async Task OnPostAsync()
@@ -48,7 +62,22 @@ namespace FrontMDD.WebApplication.Pages
             var selected = Request.Form["SelectedAbri"];
             var dateStart = Request.Form["DateStart"];
             var dateEnd = Request.Form["DateEnd"];
-            var test = selected[0];
+
+            // Récupérer la valeur de HistorySearch depuis la session
+            if (_httpContextAccessor.HttpContext.Session.TryGetValue("HistorySearch", out var historySearchDataBytes))
+            {
+                var historySearchDataString = Encoding.UTF8.GetString(historySearchDataBytes);
+                HistorySearch = JsonConvert.DeserializeObject<List<string>>(historySearchDataString);
+                if (HistorySearch.Count >= 8)
+                {
+                    HistorySearch = new List<string>();
+                }
+            }
+            else
+            {
+                HistorySearch = new List<string>();
+            }
+
             try
             {
 
@@ -60,11 +89,19 @@ namespace FrontMDD.WebApplication.Pages
                 if (AbrisStatCount > 0) 
                 {
                     Abris? libelleAbris = Abris.Find(x => x.RecordId == selected[0]);
-                    libelleResult = "Statistiques d'intéractions utilisateur pour l'abri " + libelleAbris?.Nom + " : " + AbrisStatCount;
-                } else if(AbrisStatCount == 0)
+                    var dateStartFormated = DateTime.Parse(dateStart);
+                    var dateEndFormated = DateTime.Parse(dateEnd);
+                    libelleResult = "Statistiques d'intéractions utilisateur pour l'abri " + libelleAbris?.Nom + " Du " + dateStartFormated + " Au " + dateEndFormated + " : " + AbrisStatCount;
+                    HistorySearch.Add(libelleResult);
+                    _httpContextAccessor.HttpContext.Session.SetString("HistorySearch", JsonConvert.SerializeObject(HistorySearch));
+
+                }
+                else if(AbrisStatCount == 0)
                 {
                     libelleResult = "Aucune statistiques enregistrer pendant cette période";
-                    
+                    HistorySearch.Add(libelleResult);
+                    _httpContextAccessor.HttpContext.Session.SetString("HistorySearch", JsonConvert.SerializeObject(HistorySearch));
+
                 }
 
 
@@ -81,6 +118,8 @@ namespace FrontMDD.WebApplication.Pages
             {
                 Console.WriteLine(ex);
                 libelleResult = "Le formulaire n'est pas complet";
+                HistorySearch.Add(libelleResult);
+                _httpContextAccessor.HttpContext.Session.SetString("HistorySearch", JsonConvert.SerializeObject(HistorySearch));
                 // Gérer l'erreur ici, par exemple en définissant un message d'erreur à afficher à l'utilisateur
                 ViewData["ErrorMessage"] = "Une erreur s'est produite lors du calcul des statistiques des abris.";
             }
